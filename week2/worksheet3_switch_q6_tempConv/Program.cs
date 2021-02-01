ef49026a132133352c11a67ffae4a98fa86185c6
leigh McGuinness <s00183063@mail.itsligo.ie>
2021-02-01 11:45:31 +0000

    switch_q1
    Week2-switch-q2 commit

---
+++ week2/worksheet3_switch_q6_tempConv/Program.cs
@@ -0,0 +1,49 @@
+﻿/* ===================================================================
+ * Worksheet: |  worksheet3_switch
+ * Program:   |  worksheet3_switch_q5_vowels
+ * Author:    |  Leigh McGuinness - s00183063
+ * Date:      |  01/02/21
+ * _ _ _ _ _ _| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+ * Purpose:   |  Write a program to print the following celcius/Fahrenheit
+ *            |  conversion table. 
+ *            |  
+ *            |  C is Celsius     F is Fahrenheit
+ *            |  C = 5/9 (F-32)   F = 9/5 (C+32)
+ *            |  
+ *            |  Celcius		  Fahrenheit
+ *            |  0	              32
+ *            |  10               50
+ *            |  20               68
+ *            |  ….               ...
+ *            |  …                ...
+ *            |  100              211
+ * _ _ _ _ _ _|_ _ __ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+ * Mods:      |
+ * ===================================================================*/
+
+using System;
+
+namespace worksheet3_switch_q6_tempConv
+{
+    class Program
+    {
+        // declare display tabels
+
+
+        static void Main(string[] args)
+        {
+
+            // start loop 
+
+                // 
+
+            // convert cel. to fah.
+            
+            // print column headers
+
+
+
+
+        }
+    }
+}
