bd93b0b81d349169bf984e23895cd29a679b192a
leigh McGuinness <s00183063@mail.itsligo.ie>
2021-02-13 22:40:47 +0000

    week3 - Lab 7 - arrays: addition

--- week3_arrays/array_worksheet_q2/Program.cs
+++ week3_arrays/array_worksheet_q2/Program.cs
@@ -9,7 +9,9 @@
  *            |
  *            |  Print out report of the players' stats in  a table
  * _ _ _ _ _ _|_ _ __ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
- * Mods:      | 
+ * Mods:      |  Modify the report to show the player names, 
+ *            |  1= “Pat”,2 = “John”, 3 = “Kim”, 4 = “Betty”, 5 = “Ivana”
+ *            |  Hint initialise an array with these
  *  * _ _ _ _ | _ _ _ __ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
  * BUGS:      |  
  * ===================================================================*/
@@ -27,39 +29,31 @@ namespace array_worksheet_q2
     class Program
     {
         // declare and initialize format table for output
-        public const string OUTPUT_TAB = "{0, -8}{1, 8}{2, 8}{3, 8}{4, 8}";
+        public const string OUTPUT_TAB = "{0, -12}{1, 15}{2, 15}{3, 15}{4, 15}";
         public const string INPUT_TAB = "{0, -30}{1, 5}";
+
+        //initializze the array of names
+        static public string[] namesArray = { "Pat", "John", "Kim", "Betty", "Ivana" };
 
         static void Main(string[] args)
         {
-            
-
-            // declare vars for the number of players and matches 
-            //int numPlayers, numGames;
-
-
-            ////ask user how many games and players to store stats for
-            //Console.WriteLine(INPUT_TAB,"How many players do you want to enter statistics for",": ");
-            //bool input = int.TryParse(Console.ReadLine(), out numPlayers);
-            //Console.WriteLine(INPUT_TAB, "How many matches do you want to enter statistics for", ": ");
-            //input = int.TryParse(Console.ReadLine(), out numGames);
-
             // declare a 2D array to store player goals in
             int[,] goalsArray = new int[5, 5];
+            string[] players = { "Pat", "John", "Kim", "Betty", "Ivana" };
 
-            //create a new object of Random()
-           //Random rnd = new Random();
+            //create a new object of Random() class
+            Random rnd = new Random();
 
             /* call method to fill the array with random goal stats
-             * we pass the array to fill and a Random-type object, 'rnd', which will be used 
-             * to generate the random goal stats for the array */
-            //FIllWithRandoms(goalsArray);
+             * we pass the array to fill, and a Random-type object, 'rnd',
+             * which will be used to generate the random goal stats for the array */
+            FIllWithRandoms(goalsArray, rnd);
+           
+            //print column headings
+            Console.WriteLine(OUTPUT_TAB, "Player Name", "Match 1", "Match 2", "Match 3", "Match 4");
 
-            ////print column headings
-            //Console.WriteLine(OUTPUT_TAB, "Player #", "Match 1", "Match 2","Match 3", "Match 4");
-
-            //// call method to print the table
-            //DisplayGoalStats(goalsArray);
+            // call method to print the table
+            DisplayGoalStats(goalsArray);
 
         }// END: Main()
 
@@ -67,13 +61,13 @@ namespace array_worksheet_q2
         static void FIllWithRandoms(int[,] goalsArray, Random rnd)
         {
             // fill the array with random numbers
-            for (int player = 0; player < goalsArray.Length; player++)                      // player wil represent the row
+            for (int player = 0; player < goalsArray.GetLength(0); player++)                      // player wil represent the row
             {
-                for (int goalsStats = 0; goalsStats < goalsArray.Length; goalsStats++)      // goalsStats will represent the column
+                for (int goalsStats = 0; goalsStats < goalsArray.GetLength(0); goalsStats++)      // goalsStats will represent the column
                 {
                     // assign a random number < 11 to the array @ index [player][goalsScored]
-                    // limits goals per game by a player to 10
-                    goalsArray[player, goalsStats] = rnd.Next(11);
+                    // limits goals per game by a player to 6
+                    goalsArray[player, goalsStats] = rnd.Next(7);
 
                 }//END: for(col)
 
@@ -82,23 +76,17 @@ namespace array_worksheet_q2
     
         // method to display the players and goal stats in the array
         static void DisplayGoalStats(int[,] goalsArray)
-        {
-            //const string OUTPUT_TAB = "{0, -10}{1, 8}";
-
+        {         
             // call GetLength to return the length of the row(numCols) and the number othe rows (numRows)
             int numCols = goalsArray.GetLength(0);
             int numRows = goalsArray.GetLength(1);
 
             for (int row = 0; row < numCols; row++)
             {
-                Console.Write($"Player{row}");
+                Console.Write(OUTPUT_TAB, $"{namesArray[row]}", $"{goalsArray[row,0]}", $"{goalsArray[row, 1]}", $"{goalsArray[row, 2]}", $"{goalsArray[row, 3]}");
 
-                for (int col = 0; col < numCols; col++)
-                {
-                    Console.Write($"{goalsArray[row, col]}");
-                }//END: inner for()
-            
-            }//END: outer(for)
+                Console.WriteLine();
+            }//END:(for)
         }//END: DisplayGoalStats()
 
     }//END: class
---
+++ week3_arrays/array_worksheet_q3_salesRanges/Program.cs
@@ -0,0 +1,159 @@
+﻿/* ===================================================================
+ * Worksheet: |  arrays_worksheet
+ * Program:   |  array_worksheet_q3_salesRanges
+ * Author:    |  Leigh McGuinness - s00183063
+ * Created:   |  13/02/21
+ * _ _ _ _ _ _| _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+ * Purpose:   |  A shop requires a program that determines how many customers 
+ *            |  have made purchases in each of the following ranges:
+ *            |     0-99.99 euro,
+ *            |     100-199.99 euro, 
+ *            |     200-399.99 euro, 
+ *            |     400-599.99 euro
+ *            |     600+
+ *            |  Program will print a report showing the number of sales in each 
+ *            |  of the ranges.
+ * _ _ _ _ _ _|_ _ __ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+ * Mods:      | 
+ *  * _ _ _ _ | _ _ _ __ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ 
+ * BUGS:      |  ***Not incrementig the value in salesRanf=geCount indexes
+ * ===================================================================*/
+
+
+// Library Imports.
+using System;
+using System.Linq;
+using System.Threading;
+using static System.Console;
+
+
+namespace array_worksheet_q3_salesRanges
+{
+    class Program
+    {
+        //declare and initialize format tables
+        const string INPUT_TAB = "{0, -30}{1,5}";
+        const string OUTPUT_TAB = "{0, -15}{1,25}";
+
+        //initialize public array to store the sales in
+        static public double[] salesArray = new double[100];
+        static public string[] rangeNames = { "000-99.99", "100-199.99", "200-399.99", "400-599.99", "600+" };
+        static public int[] salesRangeCount = new int[5];   //array will store the number of sales for each range
+
+        // public variable to count the sales in a range
+        static public int count = 0;
+
+        static void Main(string[] args)
+        {
+            // Encoding to allow us to print sepcial characters to the screen.
+            OutputEncoding = System.Text.Encoding.UTF8;
+
+            ReadInputs();       //call method to get sales inputs
+            PrintSales();       //call method to print the report
+
+        }//END: Main()
+
+
+        //method to read sales inputs from the user, method returns the number of values entered
+        static int ReadInputs()
+        {                                  
+            const int QUIT = -999;      //sentinel value that will allow user to exit
+            double input = 0;
+            int index = 0;
+            do
+            {
+                Console.Write(INPUT_TAB, "Enter sale amount (or -999 to quit)", ": ");
+                input = Convert.ToDouble(Console.ReadLine());
+                if (input != QUIT) { count++; }                 // only increment if value entered wasnt -999
+                CheckRange(input);                              // check ths sale catgory and inncrement its count
+                salesArray[index] = input;                      //assign th input to the array
+                index++;                                        //increment the index position
+                //test
+                //DisplayArray(salesArray);
+                //test 
+                //DisplayArray(salesRangeCount);                
+            } while (input != QUIT);                            //listen for sentinel value
+            
+            return count;
+        }// END: ReadInputs()
+
+        // method checks which sales range input is in and increments number of sales in that range
+        static void CheckRange(double input)
+        {            
+            if (input > 0 && input < 100)                          //check the value if the sale and ++ appropriate counter
+            {
+                salesRangeCount[0]++;
+                //test
+                //Console.WriteLine($"Under 100 count: {salesRangeCount[0]}");
+            }
+            else if (input >= 100 && input < 200) 
+            {
+                salesRangeCount[1]++;
+                //test
+                //Console.WriteLine($"100 to 200 count: {salesRangeCount[1]}");
+            }
+            else if (input >= 200 && input < 400) 
+            {
+                salesRangeCount[2]++;
+                //test
+                //Console.WriteLine($"200 to 400 count: {salesRangeCount[2]}");
+            }
+            else if (input >= 400 && input < 600) 
+            {
+                salesRangeCount[3]++; 
+                //test
+                //Console.WriteLine($"200 to 400 count: {salesRangeCount[3]}"); 
+            }
+            else if(input >= 600) 
+            { 
+                salesRangeCount[4]++;
+                //test
+                //Console.WriteLine($"Over 600 count: {salesRangeCount[4]}");
+            } else
+            {
+                Console.WriteLine("Invalid....sale not recorded");
+            }
+        }//END: CheckRange()
+
+        //method prints the number of sales for each sales category
+        static public void PrintSales()
+        {
+            Console.WriteLine(OUTPUT_TAB, "Sales Range", "Number of Sales");
+            for (int row = 0; row < count; row++)
+            {
+                Console.WriteLine(OUTPUT_TAB, $"{rangeNames[row]}", $"{salesRangeCount[row]}");
+            }
+        }
+        //TEST METHOD - displays the elements in an array
+        static void DisplayArray(int[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        //TEST METHOD - displays the elements in an array
+        static void DisplayArray(string[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+            Console.WriteLine();
+        }
+        //TEST METHOD - displays the elements in an array
+        static void DisplayArray(double[] arr)
+        {
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");               
+            }
+            Console.WriteLine();
+        }
+
+
+
+    }//END: class 
+
+}//END: namespace
