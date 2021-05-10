3eca3b3c261941f2eeb971c0a9b5d267627cf4ff
leigh McGuinness <s00183063@mail.itsligo.ie>
2021-05-10 21:50:57 +0100

    Create OOP-Stack-class

---
+++ training/oop/Stack/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool validInput = false;
+            int choice;
+            
+
+            do
+            {
+                Console.WriteLine("MENU:");
+                Console.WriteLine("----------------");
+                Console.WriteLine("1. PUSH AN OBJECT ONTO THE STACK");
+                Console.WriteLine("2. POP AN OBJECT OFF THE TOP OF THE STACK");
+                Console.WriteLine("3. EMPTY THE STACK");
+                Console.WriteLine("4. DISPLAY THE CURRENT STACK ORDER");
+                Console.WriteLine("\nChoicce: ");
+                string input = Console.ReadLine();
+                if (int.TryParse(input, out choice))
+                {
+                    choice = Convert.ToInt32(input);
+                    validInput = true;
+                }
+            } while (!validInput);
+
+            switch (choice)
+            {
+                case 1:
+                    StackItem item = new StackItem();
+                    stackList.Add(item.Push(item));
+                default:
+                    break;
+            }
+        }//END: Main()
+
+
+
+    }//END: Program class
+
+}
---
+++ training/oop/Stack/Stack.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Stack
+{
+    class Stack
+    {
+        static protected List<Stack> stackList = new List<Stack>();
+
+        //methods
+        public void Push(StackItem s)
+        {
+            stackList.Insert(, s);
+                
+        }
+
+        public Object Pop()
+        {
+            StackItem s = stackList[0];
+
+            return 
+        }
+
+        public void Clear()
+        {
+
+        }
+
+
+    }
+}
