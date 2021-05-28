51491760f9b3cc177ca16aacdc57e663fd07e601
Leigh McGuinness <leighmcguinness@hotmail.com>
2021-05-29 00:00:53 +0100

    create-switch() on saleAmount

--- labs_2.0/Shop/Program.cs
+++ labs_2.0/Shop/Program.cs
@@ -10,6 +10,7 @@ namespace Shop
             bool isValid = false;
             string input = "";
             double[] sales = new double[100];
+            int[] salesRanges = new int[5];      //keeps count of the instances of each sales range
             int count = 0;
             double saleAmount = 0, saleInput = 0;
 
@@ -20,13 +21,48 @@ namespace Shop
                 isValid = CheckInputIsValid(QUIT, ref isValid, input, sales, ref count, ref saleInput);
                 if (isValid)
                 {
-                    sales[count] = saleAmount;  //place saleAmount in [0] of < 100, [1] if 100 <= sale <= 200 etv.
+                    switch (saleAmount)         //check logic
+                    {
+                        case double s when saleAmount > 0 && saleAmount < 100:
+                            sales[count] = s;
+                            salesRanges[0]++;
+                            break;
+                        case double s when saleAmount < 200:
+                            sales[count] = s;
+                            salesRanges[1]++;
+                            break;
+                        case double s when saleAmount < 400:
+                            sales[count] = s;
+                            salesRanges[2]++;
+                            break;
+                        case double s when saleAmount < 600:
+                            sales[count] = s;
+                            salesRanges[3]++;
+                            break;
+                        case double s when saleAmount >= 600:
+                            sales[count] = s;
+                            salesRanges[4]++;
+                            break;
+                        default:
+                            Console.WriteLine("Sale not recorded - negative value");
+                            break;
+                    }
                     count++;
+                    
                 }
             } while (input != QUIT && !isValid);
 
 
             //print sales report
+            foreach (double sale in sales)
+            {
+                Console.Write(sale + ", ");
+            }
+
+            foreach (int range in salesRanges)
+            {
+                Console.Write(range + ", ");
+            }
 
 
 
