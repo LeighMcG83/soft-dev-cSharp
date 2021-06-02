1cf3aa6b10a99ebfbfa56f129269835f04ef08d9
Leigh McGuinness <leighmcguinness@hotmail.com>
2021-06-02 18:03:19 +0100

    Update Program.cs

--- labs_2.0/Lab8_q1_v2.0/Program.cs
+++ labs_2.0/Lab8_q1_v2.0/Program.cs
@@ -8,13 +8,13 @@ namespace Lab8_q1_v2._0
         {
             Random rnd = new Random();
             int[,] NumberArray = new int[2, 20];
-            
+
             Console.WriteLine("Rows: " + NumberArray.GetLength(0));
             Console.WriteLine("Columns : " + NumberArray.GetLength(1));
 
             for (int i = 0; i < 1; i++)         // fill 1st row with random nums
             {
-                for (int j = 0; j < cols; j++)
+                for (int j = 0; j < NumberArray.GetLength(1); j++)
                 {
                     NumberArray[i, j] = rnd.Next(0, 9);
                 }
@@ -24,6 +24,15 @@ namespace Lab8_q1_v2._0
 
             //Q1.(a)
             GetFirstOccurance(NumberArray, 7);
+
+
+            //Q1.(b)
+            /*
+             * b) Repeat the computation of part a 100 times, 
+             *    and for each position in the array, report
+             *    the number of times that the fist occurrence 
+             *    of a 7 in the array is at that position
+             */
 
             //NumberArray[1, (NumberArray[i, j] % 10) - 1]++;
 
